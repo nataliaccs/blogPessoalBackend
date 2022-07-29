9013af24ab31ec199fcda4cc6b48f7a96918ad7f
NatáliaCosta <naty1993ccs@gmail.com>
2022-07-29 18:10:09 -0300

    PrimeiraVersão

---
+++ blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagens.java
@@ -0,0 +1,56 @@
+package org.generation.blogpessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Table(name = "postagens")
+public class Postagens {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String titulo;
+	
+	private String texto;
+	
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
+	
+}
