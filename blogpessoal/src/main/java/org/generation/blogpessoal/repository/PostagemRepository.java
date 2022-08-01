b8259b094cb78972e499796284575eb3d8f5497e
NatáliaCosta <naty1993ccs@gmail.com>
2022-08-01 16:08:55 -0300

    @Param

--- blogpessoal/src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -4,9 +4,10 @@ import java.util.List;
 
 import org.generation.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
-	public List<Postagem> findAllByTituloContainingIgnoreCase (String titulo);
+	public List<Postagem> findAllByTituloContainingIgnoreCase (@Param("titulo")String titulo);
 }
