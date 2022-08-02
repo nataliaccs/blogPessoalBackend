6cde9912aecbfac6ae8fd044ab72f8510b99afe1
NatáliaCosta <naty1993ccs@gmail.com>
2022-08-02 09:30:08 -0300

    relacionamento de tabelas
    @ManyToOne

--- blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -34,6 +35,8 @@ public class Postagem {
 	@UpdateTimestamp
 	private LocalDateTime data;
 	
+	@ManyToOne
+	private Tema tema;
 	
 
 	public Long getId() {
