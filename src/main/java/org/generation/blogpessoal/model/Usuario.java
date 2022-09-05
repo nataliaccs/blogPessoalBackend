6e29ec96b82c86e3a871512c1aecea017e6a7b5c
NatáliaCosta <naty1993ccs@gmail.com>
2022-09-05 09:19:46 -0300

    construtorTipo

--- src/main/java/org/generation/blogpessoal/model/Usuario.java
+++ src/main/java/org/generation/blogpessoal/model/Usuario.java
@@ -50,12 +50,13 @@ public class Usuario {
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagem;
 		
-	public Usuario(Long id, String nome, String usuario, String senha, String foto) {
+	public Usuario(Long id, String nome, String usuario, String senha, String foto, String tipo) {
 		this.id = id;
 		this.nome = nome;
 		this.usuario = usuario;
 		this.senha = senha;
 		this.foto = foto;
+		this.tipo = tipo;
 	}
 	
 	public Usuario() { }
